e98504a27797a8374af31b37ad02f4020a2fa5ec
xhico <francgf@gmail.com>
2023-03-10 20:22:10 +0000

    Update RandomFileRename.py

--- RandomFileRename.py
+++ RandomFileRename.py
@@ -2,48 +2,36 @@
 # !/usr/bin/python3
 
 import os
-
 import random
 import string
 
 
-def random_string():
-    rndInt = random.randrange(5, 10)
-    str1 = ''.join((random.choice(string.ascii_letters) for _ in range(rndInt)))
-    str1 += ''.join((random.choice(string.digits) for _ in range(rndInt)))
-
-    # It converts the string to list.
-    sam_list = list(str1)
-
-    # It uses a random.shuffle() function to shuffle the string.
-    random.shuffle(sam_list)
-
-    # Create string
-    final_string = ''.join(sam_list)
-    return final_string
+def generate_random_string(length):
+    # generates a random string of specified length
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
 
 
-def main():
-    FOLDER = input("FOLDER: ")
+def rename_files(folder_path):
+    # get a list of all files only in the given directory
+    files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 
-    # Get list of all files only in the given directory
-    onlyFiles = filter(lambda x: os.path.isfile(os.path.join(FOLDER, x)), os.listdir(FOLDER))
-
-    # Rename files
-    for file in onlyFiles:
-        src = os.path.join(FOLDER, file)
-        dst = os.path.join(FOLDER, random_string())
-        print(src)
-        print(dst)
+    # rename files
+    for file in files:
+        src = os.path.join(folder_path, file)
+        dst = os.path.join(folder_path, generate_random_string(random.randint(5, 10)))
+        print(f'Renaming {src} to {dst}')
         os.rename(src, dst)
 
 
 if __name__ == "__main__":
     print("----------------------------------------------------")
+    # change the current working directory to the directory of the script
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
     try:
-        main()
+        folder_path = input("Enter folder path: ")
+        rename_files(folder_path)
     except Exception as ex:
         print(ex)
     finally:
